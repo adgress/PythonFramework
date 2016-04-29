1e6c5db442c7b031ca00de7b4e0d3ad1a8908fd7
Aubrey Gress <agress@ucdavis.edu>
2016-04-29 11:21:10 -0700

    fixed number of nodes

--- create_host_file.py
+++ create_host_file.py
@@ -3,6 +3,6 @@ s = 'hostfile'
 num_nodes = int(sys.argv[1])
 with open(s,'w') as f:
     f.write('master\n')
-    for i in range(num_nodes):
+    for i in range(num_nodes-1):
         n = str(i+1)
         f.write('node' + n.zfill(3) + '\n')

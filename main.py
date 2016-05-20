4a09ce0d872c48ceaaf7b99c6f7f881d3a314b76
Aubrey Gress <agress@ucdavis.edu>
2016-05-20 10:24:07 -0700

    less printing when using mpi

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def run_main(num_labels=None, split_idx=None, no_viz=None, comm=None):
 
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
-    if MPI.COMM_WORLD.Get_size() > 1:
+    if MPI.COMM_WORLD.Get_size() > 1 and mpi_utility.is_group_master():
         print '(' + socket.gethostname() + ')''Process ' + str(comm.Get_rank()) + ': Starting experiments...'
     else:
         print 'Starting experiments...'

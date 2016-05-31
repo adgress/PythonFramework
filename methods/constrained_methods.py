fixed issues with neighbor gradient

methods/constrained_methods.py
@@ -318,7 +318,7 @@ class LogisticBoundConstraint(CVXConstraint):
             if transform is not None:
                 x_c = transform.transform(x_c)
             if x is None:
-                x = np.zeros((len(constraints), x_c.shape[1]))
+                x = np.zeros((len(constraints), x_c.size))
             x[i,:] = x_c
         return x, bounds
 

methods/logistic_difference_optimize.py
@@ -75,12 +75,12 @@ class logistic_optimize(object):
     def eval(cls, data, v):
         eval_loss = cls.eval_loss(data, v)
         eval_reg = cls.eval_reg(data, v)
-        eval_mixed = cls.eval_mixed_guidance(data, v)
         reg, reg_mixed = data.get_reg()
-        v = eval_loss + reg*eval_reg
+        val = eval_loss + reg*eval_reg
         if reg_mixed > 0:
-            v += eval_mixed*reg_mixed
-        return v
+            eval_mixed = cls.eval_mixed_guidance(data, v)
+            val += eval_mixed*reg_mixed
+        return val
 
     @staticmethod
     def eval_loss(data, v):
@@ -106,7 +106,6 @@ class logistic_optimize(object):
     @classmethod
     def grad(cls, data, v):
         grad_loss = cls.grad_loss(data, v)
-        grad_mixed = cls.grad_mixed_guidance(data, v)
         reg, reg_mixed = data.get_reg()
         grad_reg = cls.grad_reg(data, v)
         grad_reg *= reg
@@ -118,6 +117,7 @@ class logistic_optimize(object):
 
         val = grad_loss + grad_reg
         if reg_mixed != 0:
+            grad_mixed = cls.grad_mixed_guidance(data, v)
             val += reg_mixed * grad_mixed
 
         return val
@@ -219,29 +219,41 @@ class logistic_pairwise(logistic_optimize):
         g[-1] *= 0
         return g
 
-class logistic_neighbor(object):
+eps = 1e-2
+
+class logistic_neighbor(logistic_optimize):
 
     @staticmethod
-    def eval_mixed_guidance(x, x_low, x_high, v):
+    def eval_mixed_guidance(data, v):
+        x = data.x_neighbor
+        x_low = data.x_low
+        x_high = data.x_high
         w, b = unpack_linear(v)
         y = apply_linear(x, w, b)
         y_low = apply_linear(x_low, w, b)
         y_high = apply_linear(x_high, w, b)
-
+        '''
+        if (y_low + eps >= y_high).any() or (y + eps >= y_high).any():
+            return np.inf
+        '''
         sig1 = sigmoid(y_high-y_low)
         sig2 = sigmoid(2*y - y_high - y_low)
         diff = sig1 - sig2
-        vals2 = -np.log(sig1-sig2)
+        #assert (np.sign(diff) > 0).all()
+        vals2 = -np.log(sig1-sig2 + eps)
         I = np.isnan(vals2)
         if I.any():
-            print 'eval_linear_neighbor_logistic: inf = ' + str(I.mean())
-            vals2[I] = 1e6
+            #print 'eval_linear_neighbor_logistic: inf = ' + str(I.mean())
+            return np.inf
         val2 = vals2.sum()
         #assert norm(val - val2)/norm(val) < 1e-6
         return val2
 
     @staticmethod
-    def grad_mixed_guidance(x, x_low, x_high, v):
+    def grad_mixed_guidance(data, v):
+        x = data.x_neighbor
+        x_low = data.x_low
+        x_high = data.x_high
         w, b = unpack_linear(v)
         y = apply_linear(x, w, b)
         y_low = apply_linear(x_low, w, b)
@@ -249,18 +261,28 @@ class logistic_neighbor(object):
 
         sig1 = sigmoid(y_high-y_low)
         sig2 = sigmoid(2*y - y_high - y_low)
-        denom = sig1 - sig2
+        denom = sig1 - sig2 + eps
         val = np.zeros(v.shape)
         for i in range(x.shape[0]):
-            num1, x1, num2, x2 = logistic_neighbor._grad_num_mixed_guidance(x[i, :], x_low[i, :], x_high[i, :], w, b)
-            val[0:-1] += (num1*x1 - num2*x2) / denom[i]
-            val[-1] += (num1 - num2) / denom[i]
+            #num1, x1, num2, x2 = logistic_neighbor._grad_num_mixed_guidance(x[i, :], x_low[i, :], x_high[i, :], w, b)
+            #val[0:-1] += (num1*x1 - num2*x2) / denom[i]
+            #val[-1] += (num1 - num2) / denom[i]
+
+            x1 = x_high[i,:] - x_low[i,:]
+            x2 = 2*x[i,:] - x_low[i,:] - x_high[i,:]
+            num1 = sig1[i]*(1-sig1[i])*x1
+            num2 = sig2[i]*(1-sig2[i])*x2
+            val[0:-1] += (num1-num2)*(1/denom[i])
+            #val[-1] += (num1-num2)*(1/denom[i])
+
         #assert not np.isnan(val).any()
         #Why isn't this necessary?
         #val *= -1
+        val[-1] = 0
+        val *= -1
         I = np.isnan(val) | np.isinf(val)
         if I.any():
-            print 'grad_linear_neighbor_logistic: nan!'
+            #print 'grad_linear_neighbor_logistic: nan!'
             val[I] = 0
         return val
 
@@ -286,52 +308,28 @@ class logistic_neighbor(object):
         return t1,x2,t2,x2
 
     @staticmethod
-    def eval(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor, v):
-        w, b = unpack_linear(v)
-        y_pred = apply_linear(x, w, b)
-        loss = eval_linear_loss_l2(x, y, v)
-        loss_neighbor = logistic_neighbor.eval_mixed_guidance(x_neighbor, x_low, x_high, v)
-        loss_reg = eval_reg_l2(w)
-
-        val = loss + reg_w*loss_reg
-        if reg_neighbor != 0:
-            val += reg_neighbor*loss_neighbor
-        return val
-
-    @staticmethod
-    def grad(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor, v):
-        w, b = unpack_linear(v)
-        grad_loss = grad_linear_loss_l2(x, y, v)
-        grad_neighbor = logistic_neighbor.grad_mixed_guidance(x_neighbor, x_low, x_high, v)
-        grad_reg = grad_reg_l2(w)
-        grad_reg *= reg_w
-        grad_reg = np.append(grad_reg, 0)
-
-        val = grad_loss + grad_reg
-        if reg_neighbor != 0:
-            val += reg_neighbor * grad_neighbor
-        return val
-
-    @staticmethod
-    def create_eval(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor):
-        return lambda v: logistic_neighbor.eval(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor, v)
-
-    @staticmethod
-    def create_grad(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor):
-        return lambda v: logistic_neighbor.grad(x, y, x_neighbor, x_low, x_high, reg_w, reg_neighbor, v)
-
-    @staticmethod
     def constraint_neighbor(v, x_low, x_high):
         w,b = unpack_linear(v)
         y_low = apply_linear(x_low,w,b)
         y_high = apply_linear(x_high,w,b)
-        return y_high - y_low
+        return y_high - y_low - eps
+
+    @staticmethod
+    def constraint_neighbor2(v, x, x_low, x_high):
+        w,b = unpack_linear(v)
+        y = apply_linear(x, w, b)
+        y_low = apply_linear(x_low,w,b)
+        y_high = apply_linear(x_high,w,b)
+        return y_high - y - eps
 
     @staticmethod
     def create_constraint_neighbor(x_low, x_high):
         return lambda v: logistic_neighbor.constraint_neighbor(v, x_low, x_high)
 
-eps = 1e-2
+    @staticmethod
+    def create_constraint_neighbor2(x, x_low, x_high):
+        return lambda v: logistic_neighbor.constraint_neighbor2(v, x, x_low, x_high)
+
 
 class logistic_bound(logistic_optimize):
     @staticmethod

methods/method.py
@@ -916,12 +916,20 @@ class RelativeRegressionMethod(Method):
             data.pairwise_relationships = set()
             #I = (data.is_train & ~data.is_labeled).nonzero()[0]
             I = data.is_train
-            pairwise_ordering = getattr(data, 'pairwise_ordering', None)
-            if pairwise_ordering is not None:
-                test_func = lambda ijk: I[ijk[0]] and I[ijk[1]] and I[ijk[2]]
-                sampled = [tuple(s.tolist()) for s in pairwise_ordering if test_func2(s)]
-                sampled = set(sampled[0:num_pairs])
+            neighbor_ordering = getattr(data, 'neighbor_ordering', None)
+            if neighbor_ordering is not None:
+                unique_func = lambda ijk: np.unique([
+                    data.true_y[ijk[0]], data.true_y[ijk[1]], data.true_y[ijk[2]]
+                ]).size == 3
+                test_func = lambda ijk: I[ijk[0]] and I[ijk[1]] and I[ijk[2]] and unique_func(ijk)
+                '''
+                for s in neighbor_ordering:
+                    print str([data.true_y[s[0]], data.true_y[s[1]], data.true_y[s[2]]])
+                '''
+                sampled = [tuple(s.tolist()) for s in neighbor_ordering if test_func(s)]
+                sampled = set(sampled[0:self.num_neighbor])
             else:
+                assert False, 'Make sure all Y are unique'
                 I = (data.is_train).nonzero()[0]
                 sampled = array_functions.sample_n_tuples(I, self.num_neighbor, 3, True)
             for i in sampled:
@@ -1026,30 +1034,43 @@ class RelativeRegressionMethod(Method):
                     data.pairwise_relationships,
                     self.transform
                 )
+                f1 = logistic_difference_optimize.logistic_neighbor.create_constraint_neighbor(x_low, x_high)
+                f2 = logistic_difference_optimize.logistic_neighbor.create_constraint_neighbor2(x_neighbor, x_low, x_high)
                 constraints = [{
                     'type': 'ineq',
-                    'fun': logistic_difference_optimize.logistic_neighbor.create_constraint_neighbor(x_low, x_high)
+                    'fun': f1
                 }]
+                constraints += [{
+                    'type': 'ineq',
+                    'fun': f2
+                }]
+
                 C = self.C
                 C4 = self.C4
-                eval = logistic_difference_optimize.logistic_neighbor.create_eval(
-                    x, y, x_neighbor, x_low, x_high, C, C4
+                opt_data = logistic_difference_optimize.optimize_data(
+                    x, y, C, C4
                 )
-                grad = logistic_difference_optimize.logistic_neighbor.create_grad(
-                    x, y, x_neighbor, x_low, x_high, C, C4
+                opt_data_feasible = logistic_difference_optimize.optimize_data(
+                    x, y, C, 0
                 )
-                eval_feasible = logistic_difference_optimize.logistic_neighbor.create_eval(
-                    x, y, x_neighbor, x_low, x_high, C, 0
-                )
-                grad_feasible = logistic_difference_optimize.logistic_neighbor.create_grad(
-                    x, y, x_neighbor, x_low, x_high, C, C4
-                )
+                opt_data.x_neighbor = x_neighbor
+                opt_data.x_low = x_low
+                opt_data.x_high = x_high
+                opt_data_feasible.x_neighbor = x_neighbor
+                opt_data_feasible.x_low = x_low
+                opt_data_feasible.x_high = x_high
+                eval = logistic_difference_optimize.logistic_neighbor.create_eval(opt_data)
+                grad = logistic_difference_optimize.logistic_neighbor.create_grad(opt_data)
+                '''
+                eval_feasible = logistic_difference_optimize.logistic_neighbor.create_eval(opt_data_feasible)
+                grad_feasible = logistic_difference_optimize.logistic_neighbor.create_grad(opt_data_feasible)
                 if not self.use_grad:
                     grad_feasible = None
                 with Capturing() as output:
                     feasible_results = optimize.minimize(eval_feasible,w0,method=method,jac=grad_feasible,options=options,constraints=constraints)
                 w0_feasible = feasible_results.x
                 w0 = w0_feasible
+                '''
             elif self.use_pairwise and self.pairwise_use_scipy:
                 x_low,x_high = PairwiseConstraint.generate_pairs_for_scipy_optimize(
                     data.pairwise_relationships,
@@ -1076,8 +1097,6 @@ class RelativeRegressionMethod(Method):
                 )
                 C = self.C
                 C2 = self.C2
-                #C = 1
-                #C2 = 1
                 s = self.s
                 opt_data = logistic_difference_optimize.optimize_data(
                     x, y, C, C2
@@ -1091,33 +1110,33 @@ class RelativeRegressionMethod(Method):
             else:
                 self.solve_cvx(x, y, data)
                 return
-
             if not self.use_grad:
                 grad = None
-            '''
+
             with Capturing() as output:
                 results = optimize.minimize(eval,w0,method=method,jac=grad,options=options,constraints=constraints)
-            '''
-            tic()
-            results = optimize.minimize(eval,w0,method=method,jac=grad,options=options,constraints=constraints)
-            toc()
 
-            tic()
-            results2 = optimize.minimize(eval,w0,method=method,jac=None,options=options,constraints=constraints)
-            toc()
+            compare_results = False
+            if compare_results:
+                tic()
+                results2 = optimize.minimize(eval,w0,method=method,jac=None,options=options,constraints=constraints)
+                toc()
+                tic()
+                results = optimize.minimize(eval,w0,method=method,jac=grad,options=options,constraints=constraints)
+                toc()
 
+                w2 = results2.x
 
-            from numpy.linalg import norm
-            print 'Error: ' + str(norm(results.x-results2.x)/norm(results.x))
-            w2 = results2.x
-            '''
-            tic()
-            self.solve_cvx(x, y, data)
-            toc()
-            w_cvx = self.w
-            b_cvx = self.b
-            print 'Error cvx: ' + str(norm(results2.x[0:-1] - w_cvx.T)/norm(w_cvx))
-            '''
+                tic()
+                self.solve_cvx(x, y, data)
+                toc()
+                w_cvx = self.w
+                b_cvx = self.b
+
+                from numpy.linalg import norm
+                print 'Error: ' + str(norm(results.x-results2.x)/norm(results.x))
+                print 'Error cvx: ' + str(norm(results2.x[0:-1] - w_cvx.T)/norm(w_cvx))
+
             w1 = results.x
             if (np.isnan(w1) | np.isinf(w1)).any():
                 w1[:] = 0
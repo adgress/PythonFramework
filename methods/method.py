952d5579ecaa70530170c4cd4ce118b2d244ee09
Aubrey Gress <agress@ucdavis.edu>
2016-06-10 18:15:38 -0700

    fixed comparing results issue

--- methods/method.py
+++ methods/method.py
@@ -1210,7 +1210,7 @@ class RelativeRegressionMethod(Method):
             #toc()
             #options['gtol'] = 1e-3
             compare_results = False
-            if compare_results or self.running_cv:
+            if compare_results or not self.running_cv:
                 results2 = optimize.minimize(eval,w0,method=method,jac=None,options=options,constraints=constraints)
                 w2 = results2.x
 

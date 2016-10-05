c04e5656fdecd556e064df284faa839f1e923dfd
adgress <agress@ucdavis.edu>
2016-10-05 15:43:17 -0700

    new normalization step for kc_housing

--- data_sets/kc_housing/process_data.py
+++ data_sets/kc_housing/process_data.py
@@ -10,6 +10,7 @@ feats_to_clear = ['id', 'date', 'yr_renovated', 'zipcode', 'lat', 'long']
 y_name = 'price'
 y_ind = array_functions.find_first_element(feat_names, y_name)
 y = data[:, y_ind].astype(np.float)
+y /= 100000
 clear_idx = array_functions.find_set(feat_names, feats_to_clear + [y_name])
 x = data[:, ~clear_idx]
 x = array_functions.remove_quotes(x)

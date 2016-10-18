ef7a6762ca8e75c22b64a3e25a29bb4f1e6b5e97
adgress <agress@ucdavis.edu>
2016-10-17 20:00:52 -0700

    select random instances for pairwise active OED

--- methods/active_methods.py
+++ methods/active_methods.py
@@ -306,7 +306,8 @@ class RelativeActiveOEDMethod(RelativeActiveMethod):
         min_pairs_to_keep = 50
 
         I = data.is_train.nonzero()[0]
-        I = I[:20]
+        I = np.random.choice(I, 20, False)
+        #I = I[:20]
         p = data.p
         all_pairs = list()
         weights = np.zeros(100000)

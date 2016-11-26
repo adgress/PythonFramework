9499b102c6fb8178885e9a5a0eae6a2762ba2a2d
adgress <agress@ucdavis.edu>
2016-11-26 15:50:39 -0800

    fixed bug if Data.y is None

--- results_class/results.py
+++ results_class/results.py
@@ -180,7 +180,7 @@ class Output(data_lib.LabeledVector):
             self.is_train = data.is_train
             self.true_y = data.true_y
             self.type = data.type
-            self.fu = np.zeros(data.y.shape)
+            self.fu = np.zeros(data.x.shape[0])
         else:
             self.y = np.empty(0)
             self.is_train = np.empty(0)

3440450091649763f568f01872e49df853976575
adgress <agress@ucdavis.edu>
2016-12-21 13:20:36 -0800

    changed bandwidth param range for CV

--- methods/far_transfer_methods.py
+++ methods/far_transfer_methods.py
@@ -109,9 +109,9 @@ class GraphTransferNW(GraphTransfer):
         super(GraphTransferNW, self).__init__(configs)
         self.cv_params = dict()
         step = 2
-        self.cv_params['C'] = self.create_cv_params(-5, 5, step, append_zero=True)
-        self.cv_params['sigma_nw'] = self.create_cv_params(-5, 5, step)
-        self.cv_params['sigma_tr'] = self.create_cv_params(-5, 5, step)
+        self.cv_params['C'] = self.create_cv_params(-5, 6, step, append_zero=True)
+        self.cv_params['sigma_nw'] = self.create_cv_params(-4, 3, 1)
+        self.cv_params['sigma_tr'] = self.create_cv_params(-9, 4, step)
         self.sigma_nw = None
         self.C = None
         self.sigma_tr = None
